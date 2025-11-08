19966059800250dcb12b41d4d8095b2265a7a302
jisu-d <lim16111611@gmail.com>
2025-11-08 22:54:28 +0900

    /{name} API 엔드포인트 추가

--- src/main/java/com/example/demo5/HelloController.java
+++ src/main/java/com/example/demo5/HelloController.java
@@ -1,14 +1,20 @@
 package com.example.demo5;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable; // PathVariable 임포트
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
     @GetMapping("/")
     public String hello() {
-        System.out.println("hello sibal11");
-        return "Hello, World! 45678dsaf";
+        System.out.println("hello root");
+        return "Hello, World!";
+    }
+
+    @GetMapping("/{name}")
+    public String helloName(@PathVariable String name) {
+        System.out.println(name);
+        return "Hello, World! " + name;
     }
 }
