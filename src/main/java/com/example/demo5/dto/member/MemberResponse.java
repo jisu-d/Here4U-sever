bc141605e8957281946e92005934c5d32a882b09
jisu-d <lim16111611@gmail.com>
2025-11-18 04:38:15 +0900

    리턴 명 바꿈

--- src/main/java/com/example/demo5/dto/member/MemberResponse.java
+++ src/main/java/com/example/demo5/dto/member/MemberResponse.java
@@ -1,16 +1,18 @@
 package com.example.demo5.dto.member;
 
 import com.example.demo5.entity.Member;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
 public class MemberResponse {
 
-    private String memberId;
+    @JsonProperty("Here4USeverNo")
+    private String here4USeverNo;
     private String phoneNumber;
 
     public MemberResponse(Member member) {
-        this.memberId = member.getMemberId();
+        this.here4USeverNo = member.getMemberId();
         this.phoneNumber = member.getPhoneNumber();
     }
 }
